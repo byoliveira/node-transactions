7777440684e0284738e624fcb3ec26592f137510
Bruno Oliveira <bruno.yuri.alves.oliveira@outlook.com>
2020-04-20 22:11:20 -0300

    Aplicação de transição financeira
    Aplicação que aborda conceitos básicos de transições financeiras "entrada/saída"

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,43 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+ // public getBalance(): Balance {
+    // TODO
+ // }
+
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
